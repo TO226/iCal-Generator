df2746bac3a8d02b06f568d445f78767a2c977e4
TO226 <gototeppen1@gmail.com>
2023-04-08 11:23:10 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# iCal Generator Chrome Extension
+
+iCal Generator is a simple Chrome extension that allows you to easily create custom iCal files.
+
+![sample-screenshot](./screenshot.png)
+
+## Features
+
+- Generate iCal files with specified event name, date, time, and location
+- Easily download the generated iCal files
+- Simple and elegant UI design inspired by Apple
+- Intuitive date picking with a popup calendar
+
+## Installation
+
+1. Clone or download this repository and save it on your local machine.
+2. In Chrome browser, navigate to the "Extensions" through the settings menu, and enable "Developer mode".
+3. Click "Load unpacked extension" button and select the cloned or downloaded folder.
+4. The extension will be installed, and its icon will appear in the Chrome extension bar.
+
+## Usage
+
+1. Click on the extension icon to open the popup.
+2. Fill in the event name, date, time, and location.
+3. Click the "Generate iCal" button to create the iCal file.
+4. Click the "Download iCal" link to download the generated iCal file.
+
+## License
+
+[MIT License](./LICENSE)
---
+++ js/popup.js
@@ -0,0 +1,67 @@
+$(function () {
+  $.datepicker.setDefaults({
+    changeMonth: "true",
+    changeYear: "true",
+    showButtonPanel: true,
+    showAnim: "clip",
+  });
+  $("#from").datepicker({});
+
+  $("#generateBtn").on("click", function () {
+    const eventName = $("#eventName").val().trim();
+    const eventLocation = $("#eventLocation").val().trim(); // 追加
+    const eventTime = $("#eventTime").val().trim();
+    const eventDate = $("#from").datepicker("getDate");
+
+    if (!eventName || !eventTime || !eventDate) {
+      alert("Please enter the event name, time, and date.");
+      return;
+    }
+
+    const eventDateTime = new Date(eventDate);
+    const [hours, minutes] = eventTime.split(":");
+    eventDateTime.setHours(hours, minutes);
+
+    const icalContent = generateICalContent(
+      eventName,
+      eventDateTime,
+      eventLocation
+    ); // 追加
+    const blob = new Blob([icalContent], {
+      type: "text/calendar;charset=utf-8",
+    });
+    const url = URL.createObjectURL(blob);
+
+    $("#downloadLink")
+      .attr("href", url)
+      .attr("download", `${eventName}.ics`)
+      .css("display", "block");
+  });
+});
+
+function formatDateForICal(dt) {
+  return dt.toISOString().replace(/[:-]/g, "").substring(0, 15) + "Z";
+}
+
+function generateICalContent(eventName, eventDateTime, eventLocation) {
+  // 追加
+  const begin = formatDateForICal(eventDateTime);
+  const end = formatDateForICal(
+    new Date(eventDateTime.getTime() + 60 * 60 * 1000)
+  );
+
+  const icalContent = [
+    "BEGIN:VCALENDAR",
+    "VERSION:2.0",
+    "BEGIN:VEVENT",
+    `DTSTART:${begin}`,
+    `DTEND:${end}`,
+    `SUMMARY:${eventName}`,
+    `LOCATION:${eventLocation}`, // 追加
+    "DESCRIPTION:Created using the iCal Generator Chrome extension.",
+    "END:VEVENT",
+    "END:VCALENDAR",
+  ].join("\n");
+
+  return icalContent;
+}
